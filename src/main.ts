dd7276842d3420358d69aa68d988dcd11f90fc85
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-02-03 14:44:35 +0900

    feat: create main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,69 @@
-import './style.css'
+import "./style.scss";
+import jszip from "jszip";
+import filesaver from "file-saver";
+import ugly from "./ugly";
+import { Buffer } from "buffer";
+import Random from "../lib/random";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+(window as any).global = window;
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+global.Buffer = global.Buffer || Buffer;
+
+const rd = new Random();
+
+function handleFileSelect(event: any) {
+  const files = event.target.files;
+
+  let file = files[0];
+
+  let reader = new FileReader();
+
+  reader.onload = (function (_f: any) {
+    return function (e: any) {
+      let contents = e.target.result;
+      jszip.loadAsync(contents).then(async function (zip) {
+        const outputzip = new jszip();
+        let index = 0;
+        zip.forEach(async (relativePath, file) => {
+          // @ts-ignore
+          await zip
+            .file(file.name)
+            .async("string")
+            .then(function (base64) {
+              if (relativePath === "project.json") {
+                outputzip.file(
+                  relativePath,
+                  JSON.stringify(ugly(JSON.parse(base64)))
+                );
+                // console.log("processing..");
+                // console.log(JSON.stringify(ugly(JSON.parse(base64))));
+              } else {
+                outputzip.file(relativePath, base64);
+              }
+            });
+
+          index += 1;
+          if (index === Object.keys(zip.files).length) {
+            outputzip
+              .generateAsync({ type: "blob" })
+              .then((reszip) => {
+                console.log(reszip);
+                filesaver.saveAs(reszip, `${rd.getRandom()}.sb3`);
+              })
+              .catch((e) => {
+                console.error(e);
+              });
+          }
+        });
+      });
+    };
+  })(file);
+
+  reader.readAsArrayBuffer(file);
+}
+
+(document.getElementById("sb3") as HTMLElement).addEventListener(
+  "change",
+  handleFileSelect,
+  false
+);
